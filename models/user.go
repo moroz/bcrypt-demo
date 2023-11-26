d6f5926030425881b6baa721c82b3eab5dc2f13f
Karol Moroz <karol@moroz.dev>
2023-11-26 22:51:09 +0800

    Add comments in Chinese

--- models/user.go
+++ models/user.go
@@ -28,24 +28,41 @@ var ARGON2_PARAMS = argon2id.Params{
 }
 
 func CreateUser(db *sqlx.DB, email, password, passwordConfirmation string) (*User, error) {
+	// 信箱不能為空
 	if email == "" {
 		return nil, errors.New("Email cannot be blank!")
 	}
+
+	// 密碼不能為空
 	if password == "" {
 		return nil, errors.New("Password cannot be blank!")
 	}
+
+	// 密碼確認必須與密碼相符
 	if password != passwordConfirmation {
 		return nil, errors.New("Passwords do not match!")
 	}
+
+	// 套用 argon2id 密碼雜湊函數
 	digest, err := argon2id.CreateHash(password, &ARGON2_PARAMS)
 	if err != nil {
 		return nil, err
 	}
-	passwordHash := string(digest)
 
 	result := User{}
-	id := uuidv7.Generate()
-	err = db.Get(&result, "insert into users (id, email, password_hash) values ($1, $2, $3) returning "+USER_COLUMNS, id.String(), email, passwordHash)
+
+	// 產生 UUIDv7
+	id := uuidv7.Generate().String()
+
+	// SQL INSERT 插入資料，用 RETURNING 即可與插入同時取得新的一筆資料
+	// 如果插入成功，result 變數裡就會是剛新增的使用者資料
+	err = db.Get(
+		&result,
+		`insert into users (id, email, password_hash)
+        values ($1, $2, $3) returning `+USER_COLUMNS,
+		// SQL 語法中三個佔位符 $1, $2, $3 需提供三個參數
+		id, email, digest,
+	)
 	return &result, err
 }
 
